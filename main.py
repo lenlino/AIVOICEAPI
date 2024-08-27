786552013935c3a83d13a42e74340d1567898a90
lenlino <65199014+lenlino@users.noreply.github.com>
2024-08-27 14:00:29 +0900

    一気に変換に変更

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 import os
 import wave
+from contextlib import asynccontextmanager
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import aiofiles
 import clr
@@ -11,42 +14,41 @@ from fastapi.responses import FileResponse, Response
 from starlette.responses import JSONResponse
 
 VOICE_DICT = [
-  {
-    "name": "琴葉 茜",
-    "styles": [
-      {
-        "name": "ノーマル",
-        "id": 0,
-        "raw_name": "琴葉 茜"
-      },
-      {
-        "name": "蕾",
-        "id": 1,
-        "raw_name": "琴葉 茜（蕾）"
-      }
-    ],
-  },
-  {
-    "name": "琴葉 葵",
-    "styles": [
-      {
-        "name": "ノーマル",
-        "id": 2,
-        "raw_name": "琴葉 葵"
-      },
-      {
-        "name": "蕾",
-        "id": 3,
-        "raw_name": "琴葉 葵（蕾）"
-      }
-    ],
-  },
+    {
+        "name": "琴葉 茜",
+        "styles": [
+            {
+                "name": "ノーマル",
+                "id": 0,
+                "raw_name": "琴葉 茜"
+            },
+            {
+                "name": "蕾",
+                "id": 1,
+                "raw_name": "琴葉 茜（蕾）"
+            }
+        ],
+    },
+    {
+        "name": "琴葉 葵",
+        "styles": [
+            {
+                "name": "ノーマル",
+                "id": 2,
+                "raw_name": "琴葉 葵"
+            },
+            {
+                "name": "蕾",
+                "id": 3,
+                "raw_name": "琴葉 葵（蕾）"
+            }
+        ],
+    },
 ]
 VOICE_DICT_FOR_GEN = {}
 for voice in VOICE_DICT:
     for style in voice["styles"]:
         VOICE_DICT_FOR_GEN[style["id"]] = style["raw_name"]
-
 
 COUNTER = 0
 
@@ -55,8 +57,8 @@ _editor_dir = os.environ['ProgramW6432'] + '\\AI\\AIVoice\\AIVoiceEditor\\'
 app = FastAPI()
 
 if not os.path.isfile(_editor_dir + 'AI.Talk.Editor.Api.dll'):
-  print("A.I.VOICE Editor (v1.3.0以降) がインストールされていません。")
-  exit()
+    print("A.I.VOICE Editor (v1.3.0以降) がインストールされていません。")
+    exit()
 
 # pythonnet DLLの読み込み
 clr.AddReference(_editor_dir + "AI.Talk.Editor.Api")
@@ -70,12 +72,15 @@ tts_control.Initialize(host_name)
 
 # A.I.VOICE Editorの起動
 if tts_control.Status == HostStatus.NotRunning:
-  tts_control.StartHost()
+    tts_control.StartHost()
 
 # A.I.VOICE Editorへ接続
 tts_control.Connect()
 host_version = tts_control.Version
 print(f"{host_name} (v{host_version}) へ接続しました。")
+
+audio_queue = []
+audio_queue_result = {}
 
 
 # テキストを設定
@@ -91,24 +96,56 @@ print(f"{host_name} (v{host_version}) へ接続しました。")
 async def get_speakers():
     return JSONResponse(content=VOICE_DICT)
 
+
 @app.post("/audio_query")
 async def synthesis(text: str, speaker: int):
-    print(tts_control.Status)
-    while tts_control.Status != HostStatus.Idle:
-        if tts_control.Status == HostStatus.NotConnected:
-            tts_control.Connect()
-        await asyncio.sleep(0.1)
-    tts_control.Text = text
-    tts_control.CurrentVoicePresetName = VOICE_DICT_FOR_GEN[speaker]
+    print("a")
+    #tts_control.CurrentVoicePresetName = VOICE_DICT_FOR_GEN[speaker]
     #play_time = tts_control.GetPlayTime()
     #tts_control.Play()
+    print(audio_queue)
+    queue_index = len(audio_queue)
+    audio_queue.append(f"{VOICE_DICT_FOR_GEN[speaker]}＞{text}")
+
+
     global COUNTER
-    COUNTER += 1
-    if COUNTER > 100:
-        COUNTER = 0
-    file_path = os.path.dirname(os.path.abspath(__file__)) + f"/audio/text{COUNTER}.wav"
-    tts_control.SaveAudioToFile(file_path)
-    async with aiofiles.open(file_path, mode='rb') as f:
+    counter_temp = COUNTER
+    while COUNTER == counter_temp:
+        await asyncio.sleep(0.1)
+    wav_file_path = audio_queue_result.pop(queue_index)
+    async with aiofiles.open(wav_file_path, mode='rb') as f:
         data: bytes = await f.read()
         return Response(data, media_type="audio/wav")
 
+
+@app.on_event("startup")
+async def skd_process():
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(starttask, "interval", seconds=1)
+    scheduler.start()
+
+
+async def starttask():
+    print(len(audio_queue))
+    if len(audio_queue) == 0:
+        return
+    if len(audio_queue_result) > 0:
+        return
+    while tts_control.Status != HostStatus.Idle:
+        print(tts_control.Status)
+        if tts_control.Status == HostStatus.NotConnected:
+            tts_control.Connect()
+        await asyncio.sleep(0.1)
+    audio_queue_result.clear()
+    global COUNTER
+    file_path = os.path.dirname(os.path.abspath(__file__)) + f"/audio/text{COUNTER}.wav"
+    texts = ""
+    for i in range(len(audio_queue)):
+        texts += f"{audio_queue[i]}\n"
+        audio_queue_result[i] = file_path.replace(f"text{COUNTER}.wav", f"text{COUNTER}-{i}.wav")
+    tts_control.Text = texts
+    audio_queue.clear()
+    tts_control.SaveAudioToFile(file_path)
+    COUNTER += 1
+    if COUNTER > 100:
+        COUNTER = 0
